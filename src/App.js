a8c1d3d2b623d88115855b27f4a40af575a1211a
Tanveer Ahamed <tanveerahemedkhan@gmail.com>
2020-01-03 12:18:15 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import {Route} from 'react-router-dom'
+import SearchBooks from './SearchBooks'
+import ListBooks from './ListBooks'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+
+class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI
+      .getAll()
+      .then(books => {
+        this.setState({books})
+      })
+  }
+
+  onShelfChange = (book, shelf) => {
+    book.shelf = shelf
+    this.setState(state => ({
+      books: state
+        .books
+        .filter(b => b.id !== book.id)
+        .concat([book])
+    }))
+    BooksAPI.update(book, shelf)
+  }
+
+  render() {
+
+    return (
+      <div className="app">
+        <Route
+          path="/"
+          exact
+          render={() => (
+          <div>
+            <div className="list-books-title">
+              <h1>My Reads</h1>
+            </div>
+            <ListBooks books={this.state.books} onShelfChange={this.onShelfChange}/>
+          </div>
+        )}/>
+
+        <Route
+          path="/search"
+          render={({history}) => (<SearchBooks
+          onShelfChange={this.onShelfChange}
+          history={history}
+          books={this.state.books}/>)}/>
+      </div>
+    )
+  }
+}
+
+export default BooksApp
---
+++ src/Book.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+class Book extends React.Component{
+    handleSubmit = (e) => {
+    e.preventDefault()
+    const shelf = e.target.value;
+    this.props.onShelfChange(this.props.book, shelf);
+  }
+
+    render() {
+      const {book} = this.props;
+        return (
+            <div className="book">
+                <div className="book-top">
+                  <div
+                      className="book-cover"
+                      style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${
+                        book.imageLinks
+                          ? book.imageLinks.thumbnail
+                          : 'icons/book-placeholder.svg'
+                      })`
+                      }}
+                    />
+                    <div className="book-shelf-changer">
+                        <select onChange={this.handleSubmit} value={book.shelf}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                     </div>
+                 </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+            </div>
+   )
+  }
+}
+
+export default Book
